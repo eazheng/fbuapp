eddfab32aabc0d3497c890aed324427794f8f03c
Daniel <danavila1020@gmail.com>
2019-07-24 11:51:18 -0700

    timeline timeago

---
+++ fbuapp/Views/CategoryHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  CategoryHeaderView.m
+//  fbuapp
+//
+//  Created by danielavila on 7/24/19.
+//  Copyright © 2019 eazheng. All rights reserved.
+//
+
+#import "CategoryHeaderView.h"
+
+@implementation CategoryHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ fbuapp/Views/PillCell.h
@@ -0,0 +1,17 @@
+//
+//  PillCell.h
+//  fbuapp
+//
+//  Created by danielavila on 7/24/19.
+//  Copyright © 2019 eazheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PillCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
